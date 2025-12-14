e3ae13409ea2130217ad0d63c53e0e9c3dd48871
iiiiswirtziiii <sethwirtz1@gmail.com>
2025-12-14 01:33:51 -0600

    Update __init__.py

--- starz_printpos/__init__.py
+++ starz_printpos/__init__.py
@@ -6,21 +6,14 @@ from .tp_tracker import (
     is_enabled,
 )
 
-
-
 from .tp_zones import (
     TPType,
     set_tp_zone,
     get_all_zones,
     clear_tp_type,
-
     delete_tp_zone,
     delete_tp_type,
     get_configured_tp_types,
     get_configured_slots,
-
     DEFAULT_ZONE_COLORS,
 )
-
-
-
